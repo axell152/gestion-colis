d8cd9259011aef2314aecb0e083ae0c57706a6e6
axell152 <axell.nicol@outlook.fr>
2026-07-20 08:50:23 +0200

    Update page.tsx

--- src/app/deplacement/page.tsx
+++ src/app/deplacement/page.tsx
@@ -1,7 +1,3 @@
 export default function DeplacementPage() {
-  return (
-    <main style={{ padding: 16 }}>
-      <h1>Déplacement de colis</h1>
-    </main>
-  )
-}
+  const [nouvelEmplacement, setNouvelEmplacement] = useState('')
+  
