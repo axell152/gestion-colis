b44e0f645421835a768f1ce88f05ee3a6ba443a1
axell152 <axell.nicol@outlook.fr>
2026-07-23 08:35:46 +0200

    Update page.tsx

--- src/app/historique/page.tsx
+++ src/app/historique/page.tsx
@@ -107,7 +107,7 @@ function detailMouvement(m: (typeof mouvements)[number]) {
             name="code"
             placeholder="Rechercher un code"
             defaultValue={searchParams.code ?? ''}
-            className={'${champClass} upprcase'}
+            className={'${champClass} uppercase'}
           />
 
           <div className="flex gap-3">
